f05318deb7d9c4d80a106ef95b09dd3d119bd1a3
sadjehwty <sadjehwty@gmail.com>
2017-12-12 16:21:21 +0100

    aggiunti dati nel db

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hero.create([
+	{ name: 'Mr. Nice' },
+	{ name: 'Narco' },
+	{ name: 'Bombasto' },
+	{ name: 'Celeritas' },
+	{ name: 'Magneta' },
+	{ name: 'RubberMan' },
+	{ name: 'Dynama' },
+	{ name: 'Dr IQ' },
+	{ name: 'Magma' },
+	{ name: 'Tornado' }])
