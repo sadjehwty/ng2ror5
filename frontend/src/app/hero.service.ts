bb567191f943101d54672a93c1354aa88840ba51
sadjehwty <sadjehwty@gmail.com>
2017-12-12 16:29:04 +0100

    da file statico a webservice

--- frontend/src/app/hero.service.ts
+++ frontend/src/app/hero.service.ts
@@ -1,25 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'http://localhost:3000/api/heroes';  // URL to web api
+  
+  constructor( private http: HttpClient, private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(heroes => this.log(`fetched heroes`)),
+      catchError(this.handleError('getHeroes', []))
+    );
   }
   
   getHero(id: number): Observable<Hero> {
-    // Todo: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+  
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
   
 }
